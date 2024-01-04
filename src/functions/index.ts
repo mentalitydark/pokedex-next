48cab8a19b46684b6365beef6d5e13f3ab8b7b41
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-01-04 20:34:27 -0300

    feat: remove pasta functions

--- src/functions/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './text-to-camel-case'
--- src/functions/text-to-camel-case.ts
+++
@@ -1,3 +0,0 @@
-export function textToCamelCase(text: string): string {
-  return text.charAt(0).toUpperCase() + text.substring(1, text.length)
-}
