d637e15725ecbd33954b6878a0fe1c2e327a65d8
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-01-10 20:43:52 -0300

    feat: adiciona link nos card e onErro para Image

--- src/app/pokemons/components/pokemon-card/pokemon-card.tsx
+++ src/app/pokemons/components/pokemon-card/pokemon-card.tsx
@@ -2,28 +2,42 @@
 
 import { Typography } from '@mui/material'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 
 import type { PokemonCardProps } from './pokemon-card-types'
 
 import { capitalizeCase, replaceCharacter } from '@/utils'
 
 export const PokemonCard = ({ name, url }: PokemonCardProps) => {
+  const [error, setError] = useState<boolean>(false)
+
   const pokemonIdRegex = new RegExp(/pokemon\/(?<id>\d*)\//)
   let id = '0'
 
   const match = url.match(pokemonIdRegex)
   if (match?.groups)
     id = match.groups.id || '0'
-  
-  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+  const fallBackImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 
   return (
-    <div className='
-      flex justify-center flex-col items-center border-2 m-2 p-4 rounded-lg cursor-pointer
-      hover:shadow-xl
-    '>
-      <Image src={imageUrl} width={96} height={96} alt={`Image of ${name}`} />
-      <Typography>{capitalizeCase(replaceCharacter(name, '-', ' '))}</Typography>
-    </div>
+    <Link href={`/pokemons/${id}`}>
+      <div className='
+        flex justify-center flex-col items-center border-2 m-2 p-4 rounded-lg cursor-pointer
+        hover:shadow-xl
+      '>
+        <Image
+          src={error ? fallBackImageUrl : imageUrl}
+          width={96}
+          height={96}
+          alt={`Image of ${name}`}
+          onError={() => setError(true)}
+        />
+        <Typography variant='body1'>{capitalizeCase(replaceCharacter(name, '-', ' '))}</Typography>
+        <Typography variant='overline'>#{id}</Typography>
+      </div>
+    </Link>
   )
 }
