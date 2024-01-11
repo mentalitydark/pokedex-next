dcc3e0f2342f6668fb1b0afc12706831529f8f3b
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-01-10 21:17:24 -0300

    style: adiciona className e altera prop mr

--- src/app/pokemons/[id]/components/attribute/attribute.tsx
+++ src/app/pokemons/[id]/components/attribute/attribute.tsx
@@ -4,8 +4,8 @@ import type { AttributeProps } from './attribute-types'
 
 export const Attribute = ({ label, value }: AttributeProps) => {
   return (
-    <Stack direction='row' spacing={.5} alignItems='center'>
-      <Typography variant='h6' mr={4}>{label}:</Typography>
+    <Stack direction='row' alignItems='center' className='border border-slate-200 rounded-full border-bg-100 px-3'>
+      <Typography className='text-lg font-semibold' mr={1}>{label}:</Typography>
       <Typography variant='subtitle1'>{value}</Typography>
     </Stack>
   )
